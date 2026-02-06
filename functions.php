47d03a792a8e78f774459f6d5ca82d10c9dc7520
Zared Rogers <zared@lightspeedwp.agency>
2026-02-06 13:44:36 +0200

    Applying fixes after review

--- functions.php
+++ functions.php
@@ -83,17 +83,15 @@ add_action( 'wp_enqueue_scripts', 'ma_theme_enqueue_assets' );
  * Enqueue editor assets.
  */
 function ma_theme_enqueue_editor_assets() {
-	wp_enqueue_style( 'wp-block-editor' );
-
 	$editor_asset_file = get_theme_file_path( 'build/css/editor-style.asset.php' );
 	if ( file_exists( $editor_asset_file ) ) {
 		$editor_asset = include $editor_asset_file;
-		       wp_enqueue_style(
-				   'ma-theme-editor-style',
-			       get_theme_file_uri( 'build/css/editor-style.css' ),
-			       $editor_asset['dependencies'] ?? array(),
-				   $editor_asset['version'] ?? MA_THEME_VERSION
-		       );
+	       wp_enqueue_style(
+			   'ma-theme-editor-style',
+	       	   get_theme_file_uri( 'build/css/editor-style.css' ),
+	       	   array_merge( array( 'wp-block-editor' ), $editor_asset['dependencies'] ?? array() ),
+			   $editor_asset['version'] ?? MA_THEME_VERSION
+	       );
 	}
 
 	// Enqueue editor JavaScript.
